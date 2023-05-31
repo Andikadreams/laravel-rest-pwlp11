051aa11d02d3a0463b2d0c196ea8c020d36640ae
Andikadreams <andikawibowoe87@gmail.com>
2023-05-31 12:32:27 +0700

    Jobsheet 13 Praktikum 3

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
-use App\Http\Resources\LoginResource;
 use Illuminate\Support\Facades\Hash;
+use App\Http\Resources\LoginResource;
+use App\Http\Requests\RegisterRequest;
 
 class ApiAuthController extends Controller
 {
@@ -42,5 +43,22 @@ class ApiAuthController extends Controller
 
         // Response
         return response()->noContent();
+    }
+
+    public function register(RegisterRequest $request){
+        $user = User::create([
+            'username' => $request->username,
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        return new LoginResource([
+            'message' => 'success login',
+            'user' => $user,
+            'token' => $token,
+        ],200);
     }
 }
